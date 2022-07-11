bbcab6526a3d570082ebe2bbf79c2dc0580075e3
OQueLucas <lucasestevam365@gmail.com>
2022-07-11 11:13:03 -0300

    Creating components functions to observe the functions behaviors of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Twitter from './Twitter.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: false,
+    actived: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: true
+      })
+    }, 3000)
+  }
+
+  onRemove = () => {
+    this.setState({
+      actived: false
+    })
+  }
+
+  render () {
+    const post = [{
+      title: 'xpto',
+      description: 'foo'
+    },{
+      title: 'xpto',
+      description: 'foo'
+    }]
+
+    return (
+      <div>
+        <button onClick={this.onRemove}>Remover component</button>
+        {this.state.actived && (
+          <Twitter posts={post} loading={this.state.loading} />
+        )}
+      </div>
+      )
+  }
 }
 
 export default App;
